4d7b4ba59be4528384f2c0cc9a8c24be7eb4a3e6
trickwide <82200433+trickwide@users.noreply.github.com>
2023-04-11 19:04:08 +0300

    Adding progress from exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("tiny"));
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 app.use(express.json());
 
 let persons = [

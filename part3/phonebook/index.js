013525bf35b63b37aef84a7d8b236849f91d758e
Ville Saastamoinen <trickwide@gmail.com>
2024-01-31 14:53:37 +0200

    Add submission for exercise 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -85,11 +85,12 @@ app.get("/api/persons/:id", (request, response) => {
   });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const PORT = process.env.PORT;

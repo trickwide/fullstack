50a8dfd5e8ef068e75a303a4c16c0465d27d8b06
Ville Saastamoinen <trickwide@gmail.com>
2024-01-31 15:40:41 +0200

    Add submissions for exercises 3.17.-3.18 and fix error handling

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -22,7 +22,9 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
-app.use(errorHandler);
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -51,25 +53,33 @@ app.post("/api/persons", (request, response) => {
     name: body.name,
     number: body.number,
   });
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
   const date = new Date();
-  const info = `<p>Phonebook has info for ${persons.length} people</p>
-  <p>${date}</p>`;
-  response.send(info);
+  Person.find({}).then((persons) => {
+    const info = `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`;
+    response.send(info);
+  });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (!person) {
-      response.status(404).end();
-    }
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
@@ -79,6 +89,20 @@ app.delete("/api/persons/:id", (request, response, next) => {
     })
     .catch((error) => next(error));
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = { name: body.name, number: body.number };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {

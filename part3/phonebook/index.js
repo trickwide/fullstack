436eb8f3bea66640de9c085fcdf982d5e98ea89a
Ville Saastamoinen <trickwide@gmail.com>
2024-01-31 15:16:32 +0200

    Add submission for exercise 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -14,28 +14,15 @@ app.use(
 app.use(express.static("dist"));
 app.use(express.json());
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {

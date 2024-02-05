46ed529403bd4e815debf35dfbd9da0bf94f0042
Ville Saastamoinen <trickwide@gmail.com>
2024-02-05 15:04:37 +0200

    Add submission for exercise 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})

48cbf081ddc423a5e590f7666f3bac0285a01b90
Ville Saastamoinen <trickwide@gmail.com>
2024-02-05 15:42:47 +0200

    Add submission for exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -14,24 +14,73 @@ beforeEach(async () => {
     await blogObject.save()
   }
 })
+describe('GET-requests', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('UID is defined as id instead of database default _id', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('POST-requests', () => {
+  test('New blog is added succesfully and total number of blogs is increased by one', async () => {
+    const newBlog = {
+      title: 'Test blog',
+      author: 'Test author',
+      url: 'http://www.test.com',
+      likes: 0,
+    }
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-test('UID is defined as id instead of database default _id', async () => {
-  const response = await api.get('/api/blogs')
+    const response = await api.get('/api/blogs')
 
-  expect(response.body[0].id).toBeDefined()
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  })
+
+  test('New blog is added with correct content', async () => {
+    const newBlog = {
+      title: 'Test blog',
+      author: 'Test author',
+      url: 'http://www.test.com',
+      likes: 0,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const blogsAfterPost = response.body
+
+    const addedBlog = blogsAfterPost.find(
+      (blog) => blog.title === newBlog.title
+    )
+
+    expect(addedBlog.title).toBe(newBlog.title)
+    expect(addedBlog.author).toBe(newBlog.author)
+    expect(addedBlog.url).toBe(newBlog.url)
+    expect(addedBlog.likes).toBe(newBlog.likes)
+  })
 })
 
 afterAll(async () => {

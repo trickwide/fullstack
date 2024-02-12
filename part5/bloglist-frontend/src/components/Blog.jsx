a2967662e868381feeb7d53531cacef3905394f9
Ville Saastamoinen <trickwide@gmail.com>
2024-02-12 16:27:44 +0200

    Add submission for exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      {visible && (
+        <div>
+          {blog.url}
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog

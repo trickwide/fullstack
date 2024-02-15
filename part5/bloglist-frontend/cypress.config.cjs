78fa5452028b59bb0d2acab3258179138a09f7e8
Ville Saastamoinen <trickwide@gmail.com>
2024-02-15 17:19:11 +0200

    Add submission for exercise 5.18

---
+++ part5/bloglist-frontend/cypress.config.cjs
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {},
+
+    baseUrl: 'http://localhost:5173',
+    env: {
+      BACKEND: 'http://localhost:3003/api',
+    },
+  },
+})
--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,7 +1,13 @@
 describe('Blog app', function () {
   beforeEach(function () {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:5173')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      name: 'Test User',
+      username: 'tuser',
+      password: 'secret',
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+    cy.visit('')
   })
 
   it('Login form is shown', function () {
@@ -9,5 +15,24 @@ describe('Blog app', function () {
     cy.get('#username')
     cy.get('#password')
     cy.get('#login-button').contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('tuser')
+      cy.get('#password').type('secret')
+      cy.get('#login-button').click()
+
+      cy.contains('tuser logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('tuser')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error').contains('Wrong username or password')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })

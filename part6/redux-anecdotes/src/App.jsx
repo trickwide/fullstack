0e09e595c18654e8ede392291a8041bd0c1cbfd4
Ville Saastamoinen <trickwide@gmail.com>
2024-02-22 15:04:46 +0200

    Add submission for exercises 6.16 and 6.17

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,16 +1,13 @@
 import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
-import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, [])
   return (
     <div>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.jsx
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -11,9 +12,6 @@ const anecdoteSlice = createSlice({
           : { ...anecdote, votes: anecdote.votes + 1 }
       )
     },
-    createAnecdote: (state, action) => {
-      state.push(action.payload)
-    },
     appendAnecdote(state, action) {
       state.push(action.payload)
     },
@@ -23,11 +21,20 @@ const anecdoteSlice = createSlice({
   },
 })
 
-export const {
-  voteFor,
-  createAnecdote,
-  appendAnecdote,
-  setAnecdotes,
-} = anecdoteSlice.actions
+export const { voteFor, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
+  }
+}
 
 export default anecdoteSlice.reducer
